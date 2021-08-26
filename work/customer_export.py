a9e963fcedf2c47c929ae6a604d203b8bde34f02
axue <271283046@qq.com>
2021-08-26 10:43:56 +0800

    初始化项目

--- work/customer_export.py
+++ work/customer_export.py
@@ -8,6 +8,8 @@ ws = workbook["Sheet5"]
 if __name__ == "__main__":
     for i in ws.rows:
         s = str(i[0].value)
-        print(s)
-        sql = "INSERT INTO saas_solution_meter_reading.t_customer (tenement_code, project_code, customer_name, lease_status, version, create_time, create_by, update_time, update_by, logic_del) VALUES ('ZH_00001', 'ZH_00001_XM_00000001', '%s', '0', 1, '2021-08-24 14:15:38', null, '2021-08-24 14:15:42', null, 0)" % s
+        sql = "INSERT INTO saas_solution_meter_reading.t_customer (tenement_code, project_code, customer_name, " \
+              "lease_status, version, create_time, create_by, update_time, update_by, logic_del) VALUES " \
+              "('ZH_00001', 'ZH_00001_XM_00000001', '%s', '0', 1, '2021-08-24 14:15:38', null, " \
+              "'2021-08-24 14:15:42', null, 0)" % s
         MysqlUtil().save(sql)
